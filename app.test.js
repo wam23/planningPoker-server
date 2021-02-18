d23543946188ef6b0773f254c05fe83a25fc653c
Rafael Wampfler <wampfler@gmail.com>
2021-02-18 10:15:15 +0100

    better expect syntax

--- app.test.js
+++ app.test.js
@@ -8,35 +8,46 @@ beforeEach(() => {
 
 beforeEach(async () => {
     // reset room
-    await request(app).get("/rooms/1/reset");
+    await request(app)
+        .get("/rooms/1/reset");
+
     // prefill with 1 vote
-    await request(app).post("/rooms/1/vote").send({ name: 'Test', vote: 3 });
+    await request(app)
+        .post("/rooms/1/vote")
+        .send({ name: 'Test', vote: 3 });
 });
 
 test("GET / should respond text", async () => {
-    const response = await request(app).get("/");
-    expect(response.text).toEqual("Hello world from A-Team");
-    expect(response.statusCode).toBe(200);
+    await request(app)
+        .get("/")
+        .expect(200)
+        .expect("Hello world from A-Team");
 });
 
 test("POST /vote should respond created", async () => {
-    const response = await request(app).post("/rooms/1/vote").send({ name: 'Test2', vote: 5 });
-    expect(response.statusCode).toBe(201);
+    await request(app)
+        .post("/rooms/1/vote")
+        .send({ name: 'Test2', vote: 5 })
+        .expect(201);
 
-    const response2 = await request(app).get("/rooms/1/votes");
-    expect(response2.text).toEqual("{\"Test\":3,\"Test2\":5}");
+    await request(app)
+        .get("/rooms/1/votes")
+        .expect("{\"Test\":3,\"Test2\":5}");
 });
 
 test("GET /votes should respond votes", async () => {
-    const response = await request(app).get("/rooms/1/votes");
-    expect(response.text).toEqual("{\"Test\":3}");
-    expect(response.statusCode).toBe(200);
+    await request(app)
+        .get("/rooms/1/votes")
+        .expect(200)
+        .expect("{\"Test\":3}");
 });
 
 test("GET /reset should respond no content", async () => {
-    const response = await request(app).get("/rooms/1/reset");
-    expect(response.statusCode).toBe(204);
+    await request(app)
+        .get("/rooms/1/reset")
+        .expect(204);
 
-    const response2 = await request(app).get("/rooms/1/votes");
-    expect(response2.text).toEqual("{}");
+    await request(app)
+        .get("/rooms/1/votes")
+        .expect("{}");
 });
