49e65d6866c1851a6c635e4746d57678758b9dc7
U117331 <U117331@mobi.ch>
2022-08-18 07:16:32 +0200

    show room bei init rollback

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/poll/:room/init', (req, res) => {
    if (!global.express_longpoll_emitters[url]) {
       longpoll.create(url);
    }
-   const result = votesAsArray(req.params.room.toLowerCase(), true);
+   const result = votesAsArray(req.params.room.toLowerCase(), false);
    res.send(result);
 });
 
